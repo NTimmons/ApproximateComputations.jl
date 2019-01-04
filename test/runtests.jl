c291b8047696d6500b1b9686a462541c51f6dfee
Nicholas Timmons <ngtimmons@gmail.com>
2019-01-04 12:59:49 +0000

    memo tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,3 +48,29 @@ end
 	@test newfunc() == 8
 
 end
+
+@testset "Memoisation" begin
+
+	# Test custom overwrite hashing
+	sinhash(fn, val) = hash(val)
+	memoDict = Dict()
+	ApproximateHashingMemoise(sin, memoDict, sinhash, 0.4)
+	ApproximateHashingMemoise(sin, memoDict, sinhash, 0.1)
+	
+	@test ApproximateHashingMemoise(sin, memoDict, sinhash, 0.4) == 0.3894183423086505
+	@test ApproximateHashingMemoise(sin, memoDict, sinhash, 0.1) == 0.09983341664682815
+
+	# Test trending batched hash values
+	trendingArray = []
+	for i in 1:5000
+	   push!(trendingArray,[0,0.0,0.0]) 
+	end
+
+	trendingsinhash(fn, val) = 1+Int64.(round(val*10.0))
+
+	@test TrendingMemoisation(sin, trendingsinhash, trendingArray, 3, 0.5)  == 0.479425538604203
+	@test TrendingMemoisation(sin, trendingsinhash, trendingArray, 3, 0.55) == 0.5226872289306592
+	@test TrendingMemoisation(sin, trendingsinhash, trendingArray, 3, 0.51) == 0.48817724688290753
+	@test TrendingMemoisation(sin, trendingsinhash, trendingArray, 3, 0.59) == 0.5563610229127838
+	@test TrendingMemoisation(sin, trendingsinhash, trendingArray, 3, 0.59) == 0.5563610229127838
+end
